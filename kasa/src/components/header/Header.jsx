342267e03b9a34237d6a42a4bd3683127b4b3193
RandumGoo <166697600+RandumGoo@users.noreply.github.com>
2024-08-23 16:08:04 +0200

    Ajout du header

---
+++ kasa/src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+// Header.jsx
+import Logo from "../../img/LOGO.svg";
+import "./Header.scss"
+import Navbar from '../navbar/Navbar'
+
+export default function Header() {
+	return (
+		<header className='header'>
+			<h1>
+				<img src={Logo} alt="kasa, location d'appartements" />
+			</h1>
+			<Navbar />
+		</header>
+	)
+}
